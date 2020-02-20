3af0e5bb72238bcf7c76934cc31b8e9784b86ef8
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-02-20 11:02:49 +0800

    Create list_products.php

---
+++ list_products.php
@@ -0,0 +1,26 @@
+<?php
+$products = file_get_contents('http://localhost/json/json-1.php');
+
+$details = json_decode($products,true);
+
+$list = $details['records'];
+?>
+
+<table> 
+  <tr>
+    <td> Product </td>
+    <td> Price </td>
+    <td> Category </td>
+  </tr>
+<?php
+foreach($list as $value){
+?>
+  <tr> 
+    <td><?php echo $value['name']; ?></td>
+    <td><?php echo $value['price']; ?></td>
+    <td><?php echo $value['category_name']; ?></td>
+  </tr>
+<?php
+}
+?>
+</table>
