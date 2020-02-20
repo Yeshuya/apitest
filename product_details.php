9ae6b5f8b9239d2526e95c9f7b6993771b32504b
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-02-20 11:18:59 +0800

    Create product_details.php

---
+++ product_details.php
@@ -0,0 +1,26 @@
+<?php
+$products = file_get_contents('http://rdapi.herokuapp.com/product/read.php?id='.$id);
+
+$details = json_decode($products,true);
+
+$list = $details['records'];
+?>
+
+<table> 
+  <tr>
+    <td> Product Name </td>
+    <td> <?php echo $list['name'];?> </td>
+  <tr>
+  <tr>
+    <td> Description </td>
+    <td> <?php echo $list['description'];?> </td>
+  <tr>
+  <tr>
+    <td> Price </td>
+    <td> <?php echo $list['price'];?> </td>
+  <tr>
+  <tr>
+    <td> Category </td>
+    <td> <?php echo $list['category_name'];?> </td>
+  <tr>
+</table>
