feae35287b032490df70f6f9e5f89fec167efc07
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-02-20 11:03:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <table>
   <tr>
     <td><a href = "index.php"> Home </a></td>
-    <td><a href="index.php?page=Products"> Products </a></td>
+    <td><a href="list_products.php"> Products </a></td>
     <td><a href="index.php?page=Category"> Category </a></td>
   </tr>
 </table>
